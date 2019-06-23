e527fda090ac3d90a92d19de8ac36b9034413b74
József Tar <szurilo@gmail.com>
2019-06-23 22:29:17 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.2d41bec160de88d6041456865c6ab323.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "e52ed7b5938202c5fc1bb618761bfac7",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "8a86074be2c98cd1bab4",
+    "url": "/website/js/about.3ce9cd3f.js"
+  },
+  {
+    "revision": "0ce85a90167d982d7cba",
+    "url": "/website/js/app.f9a39b23.js"
+  },
+  {
+    "revision": "c0999c6352c59a88da32",
+    "url": "/website/js/chunk-vendors.eea671b0.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/website/robots.txt"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/website/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "2e8f8f89fa669d46992410190af06a47",
+    "url": "/website/img/theme/wikipedia.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/website/brand/favicon.png"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/website/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "fb2814ddd5facbdaca9b610a361b0bc3",
+    "url": "/website/img/theme/wedding.png"
+  },
+  {
+    "revision": "93cdbf4168a449b8701022dccdc50b18",
+    "url": "/website/img/theme/szabodavid.jpg"
+  },
+  {
+    "revision": "51bb6c5e1f2867f789f67856aa1f80d4",
+    "url": "/website/img/theme/wellochat.logo.svg"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/website/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/website/img/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/website/img/brand/favicon.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/website/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/website/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/website/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/website/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/website/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/website/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/website/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/website/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/website/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "0ce85a90167d982d7cba",
+    "url": "/website/css/app.84199ced.css"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/website/brand/white.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/website/brand/github-white-slim.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/website/brand/blue.png"
+  }
+];
